e7c17ede32744019b6f2598b4fb47fdd20abb17b
Surya <suryanandhv@gmail.com>
2017-11-26 08:33:32 -0600

    Initial commit from my pc

--- messenger/src/main/java/org/surya/javabrains/messenger/MyResource.java
+++ messenger/src/main/java/org/surya/javabrains/messenger/MyResource.java
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Got it Hello World - I am good here!";
     }
 }
